543330ecaea99e23e82e5e497d1e4db94f22be4d
Tomsylva <Tomsylvanz@gmail.com>
2021-04-06 09:47:41 +0200

    community links are live

--- routes/community.js
+++ routes/community.js
@@ -5,4 +5,17 @@ router.get("/", (req, res) => {
   res.render("community/community-home");
 });
 
+router.get("/:dynamicCommunity", (req, res) => {
+  Community.findOne({ slug: req.params.dynamicCommunity }).then(
+    (singleCommunity) => {
+      if (!singleCommunity) {
+        return res.redirect("/");
+      }
+      res.render("community/single-community", {
+        singleCommunity: singleCommunity,
+      });
+    }
+  );
+});
+
 module.exports = router;
