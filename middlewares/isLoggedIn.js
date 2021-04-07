d528aaf3d641a9711710a21aca2e2ff2c1306b0b
jahfunk <xknottykidx@gmail.com>
2021-04-07 16:01:48 +0200

    auth

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,11 +1,7 @@
 module.exports = (req, res, next) => {
   // checks if the user is logged in when trying to access a specific page
   if (!req.session.user) {
-<<<<<<< HEAD
     return res.redirect("auth/login");
-=======
-    return res.redirect("/auth/login");
->>>>>>> tom-changes
   }
   req.user = req.session.user;
   next();
